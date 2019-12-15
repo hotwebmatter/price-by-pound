3a5f89e7781a3946077c38203f4348ab763b329f
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-15 14:25:32 -0500

    Initial commit of price-by-pound project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace price_by_pound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Price by Pound
